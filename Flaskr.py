e737e2e4f73b8f42bf9289adbb31dce7b0ffaac1
Li Jian <lijian@hantek.com>
2016-12-02 14:25:03 +0800

    可以运行起来

---
+++ Flaskr.py
@@ -0,0 +1,32 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+    ))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+app.run('localhost', '5555')
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
